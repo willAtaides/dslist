fc8e4ef9a171a9cb98a6c94a3becf81353f5a310
Willian Ataides <sg552.santos@gmail.com>
2024-01-09 05:16:52 -0300

    GET game by id

--- src/main/java/com/listgames/dslist/controllers/GameController.java
+++ src/main/java/com/listgames/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.listgames.dslist.dto.GameDTO;
 import com.listgames.dslist.dto.GameMinDTO;
 import com.listgames.dslist.services.GameService;
 
@@ -17,6 +19,13 @@ public class GameController {
 	
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO result = gameService.findByid(id);
+		return result;
+	}
+	
 	
 	@GetMapping
 	public List<GameMinDTO> findall(){
--- src/main/java/com/listgames/dslist/services/GameService.java
+++ src/main/java/com/listgames/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.listgames.dslist.dto.GameDTO;
 import com.listgames.dslist.dto.GameMinDTO;
 import com.listgames.dslist.entities.Game;
 import com.listgames.dslist.repositories.GameRepository;
@@ -15,6 +17,13 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public GameDTO findByid(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return  new GameDTO(result);
+	}
+	
 	
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
